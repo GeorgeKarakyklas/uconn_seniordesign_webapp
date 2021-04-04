23f99cc6219ab0cc453ead1c703da08d7edf28da
charlesira <charlie.ira@gmail.com>
2021-04-03 21:49:15 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react'
+import {Home} from './pages/Home';
+import {About} from './pages/About';
+import {ContactUs} from './pages/ContactUs';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
+import { Documentation } from './pages/Documentation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <NavigationBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contactUs" component={ContactUs} />
+          <Route path="/docs" component={Documentation} />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
