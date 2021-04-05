d372f0f8280e54b4fa07ebc6ed8e110b4b3da0b5
charlesira <charlie.ira@gmail.com>
2021-04-05 01:40:47 -0400

    added photos and updated pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,19 +3,25 @@ import styled from 'styled-components';
 
 const GridWrapper = styled.div`
   display: grid;
-  grid-gap: 10px;
+  grid-gap: 20px;
   margin-top: 1em;
   margin-left: 6em;
   margin-right: 6em;
-  grid-template-columns: repeat(12, 1fr);
-  grid-auto-rows: minmax(25px, auto);
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(300px, auto);
 `; 
 
 export const About = () => (
   <GridWrapper>
-    <h2>About Page</h2>
-    <p>State at ceiling lay on arms while you're using the keyboard so this human feeds me.</p>
-    <p>I am a kitty cat, sup, feed me, no cares in the world</p>
-    <p>Meow meow, I tell my human purr for no reason but to chase after</p>
+    <h2>About The Project</h2>
+    <div>
+        <a href = "https://zap.org/home" rel = "noreferrer" target = "_blank">
+            <img src = {"/images/zap_logo.jpeg"} alt = "zap" width = "700" height = "300"></img>
+        </a>
+        <a href = "https://seniordesign.engr.uconn.edu/" rel = "noreferrer" target = "_blank">
+            <img src = {"/images/uconn_logo.jpg"} alt = "uconn"></img>
+        </a>
+    </div>
+    <p>This is a Senior Design Project being in done in conjuction with the Zap Team from the NY Blockchain Center. Our aim is to help expand and prove the usability of the Zap protocol. </p>
   </GridWrapper>
 )
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -3,19 +3,46 @@ import styled from 'styled-components';
 
 const GridWrapper = styled.div`
   display: grid;
-  grid-gap: 10px;
+  grid-gap: 20px;
   margin-top: 1em;
   margin-left: 6em;
   margin-right: 6em;
-  grid-template-columns: repeat(12, 1fr);
-  grid-auto-rows: minmax(25px, auto);
+  grid-template-columns: repeat(6, 1fr);
+  grid-auto-rows: minmax(100px, auto);
 `; 
 
 export const ContactUs = () => (
   <GridWrapper>
     <h2>Contact Us</h2>
-    <p>State at ceiling lay on arms while you're using the keyboard so this human feeds me.</p>
-    <p>I am a kitty cat, sup, feed me, no cares in the world</p>
-    <p>Meow meow, I tell my human purr for no reason but to chase after</p>
+    <div>
+        <img src = {"/images/placeholder.png"} alt = "face1"></img>
+        <p>Charles Ira</p>
+        <p>charles.ira@uconn.edu</p>
+        <a href = "https://github.com/charlesira" rel = "noreferrer" target = "_blank">Github: charlesira</a> 
+    </div>
+    <div>
+        <img src = {"/images/placeholder.png"} alt = "face2"></img>
+        <p>Georgios Karakyklas</p>
+        <p>georgios.karakyklas@uconn.edu</p>
+        <a href = "https://github.com/GeorgeKarakyklas/" rel = "noreferrer" target = "_blank">Github: GeorgeKarakyklas</a> 
+    </div>
+    <div>
+        <img src = {"/images/placeholder.png"} alt = "face3"></img>
+        <p>Robin Lopez</p>
+        <p>robin.lopez@uconn.edu</p>
+        <a href = "https://github.com/charlesira" rel = "noreferrer" target = "_blank">Github: charlesira</a> 
+    </div>
+    <div>
+        <img src = {"/images/placeholder.png"} alt = "face4"></img>
+        <p>Jason Chau</p>
+        <p>jason.chau@uconn.edu</p>
+        <a href = "https://github.com/Jason-Chau" rel = "noreferrer" target = "_blank">Github: Jason-Chau</a> 
+    </div>
+    <div>
+        <img src = {"/images/placeholder.png"} alt = "face5"></img>
+        <p>Gaurav Manandhar</p>
+        <p>gaurav.manandhar@uconn.edu</p>
+        <a href = "https://github.com/gauravm1997" rel = "noreferrer" target = "_blank">Github: guaravm1997</a> 
+    </div>
   </GridWrapper>
 )
